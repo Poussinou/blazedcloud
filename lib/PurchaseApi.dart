703345e9232b83771cdcd4296d21baec612de0f2
TheRedSpy15 <hjadar15@protonmail.com>
2023-10-25 00:06:44 -0400

    replace logger()

--- lib/PurchaseApi.dart
+++ lib/PurchaseApi.dart
@@ -1,17 +1,14 @@
+import 'package:blazedcloud/log.dart';
 import 'package:glassfy_flutter/glassfy_flutter.dart';
 import 'package:glassfy_flutter/models.dart';
-import 'package:logging/logging.dart';
 
 class PurchaseApi {
-  static const _apiKey = '821feb96c6024f2b9db92964b6726cda';
-
   static Future<List<GlassfyOffering>> fetchOffers() async {
-    final log = Logger("main");
     try {
       final offerings = await Glassfy.offerings();
       return offerings.all ?? [];
     } catch (e) {
-      log.warning("Glassfy failed to fetch offers: $e");
+      logger.w("Glassfy failed to fetch offers: $e");
       return [];
     }
   }
