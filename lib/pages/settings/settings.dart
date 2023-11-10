dc06bac017ce99261d0956bff372cca1b8915789
TheRedSpy15 <hjadar15@protonmail.com>
2023-11-10 00:16:57 -0500

    display current download directory in settings

--- lib/pages/settings/settings.dart
+++ lib/pages/settings/settings.dart
@@ -142,7 +142,7 @@ class SettingsScreen extends ConsumerWidget {
       trailing: const SizedBox.shrink(),
       iconStyle: babstrap.IconStyle(),
       title: 'Change Download Location',
-      subtitle: "Current: ",
+      subtitle: "Current: ${getExportDirectoryFromHive()}",
     );
   }
 
